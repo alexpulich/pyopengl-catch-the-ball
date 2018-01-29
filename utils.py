7d75a57ecb4a4e5b4be63d040f9d55599cbfcc08
Alex Pulich <alexpulich@gmail.com>
2018-01-29 21:46:53 +0300

    utils refactoring, removed deprecated method, docs

--- utils.py
+++ utils.py
@@ -1,38 +1,28 @@
+""" Module for different helpers. Currently there is only one which is a texture loader"""
 from OpenGL.GL import *
 from PIL import Image
 import numpy
 
 
 class Textureloader:
-
     @staticmethod
-    def load_texture(path):
+    def load(path):
+        """ takes a path to resource with texture (e.g. resources/bucket.jpg) and returns a named texture """
         texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, texture)
+
         # Set the texture wrapping parameters
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+
         # Set texture filtering parameters
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        # load image
-        image = Image.open(path)
-        img_data = numpy.array(list(image.getdata()), numpy.uint8)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
-        return texture
 
-    @staticmethod
-    def load_text_texture(path):
-        texture = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, texture)
-        # Set the texture wrapping parameters
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        # Set texture filtering parameters
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
         # load image
         image = Image.open(path)
         img_data = numpy.array(list(image.getdata()), numpy.uint8)
+
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+
         return texture
